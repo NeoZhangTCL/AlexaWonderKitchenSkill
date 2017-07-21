da3ab8ffd16bd42f40fbdf2e4abbe30fb9a0f961
Neo Zhang <tuochaoz@amazon.com>
2017-07-21 17:58:10 -0400

    revert sample and edit wonder_kitchen.py

---
+++ src/wonder_kitchen.py
@@ -0,0 +1,150 @@
+from __future__ import print_function
+
+# --------------- Static Info ----------------------
+
+outputs = {
+            "welcome" : """Welcome to the Wonder Kitchen.
+                Please tell me what ingredients you have.
+                Like I have egg, or I have milk""",
+            "end" : """Thank you for trying the Wonder Kitchen.
+                Have a nice day! """,
+            "reply" : """Here is an suggestion""",
+            "change" : """Ok, here is the new recipe"""
+        }
+
+card_titles = {
+                "welcome" : "Welcome",
+                "end" : "Goodbye",
+                "reply" : "Recipe",
+                "change" : "Recipe"
+            }
+
+# --------------- Helpers that build all of the responses ----------------------
+
+def build_speechlet_response(session, should_end_session):
+    return {
+        'outputSpeech': {
+            'type': 'PlainText',
+            'text': outputs[session]
+        },
+        'card': {
+            'type': 'Simple',
+            'title': "Wonder Kitchen - " + card_titles[session],
+            'content': "Wonder Kitchen - " + outputs[session]
+        },
+        'reprompt': {
+            'outputSpeech': {
+                'type': 'PlainText',
+                'text': "Are you still there?"
+            }
+        },
+        'shouldEndSession': should_end_session
+    }
+
+
+def build_response(session_attributes, speechlet_response):
+    return {
+        'version': '1.0',
+        'sessionAttributes': session_attributes,
+        'response': speechlet_response
+    }
+
+
+# --------------- Functions that control the skill's behavior ------------------
+
+def welcome_info():
+    session = "welcome"
+    should_end_session = False
+    return build_response({}, build_speechlet_response(
+        session, should_end_session))
+
+
+def end_info():
+    session = "end"
+    should_end_session = True
+    return build_response({}, build_speechlet_response(
+        session, should_end_session))
+
+
+
+
+# --------------- Events ------------------
+
+def on_session_started(session_started_request, session):
+    """ Called when the session starts """
+
+    print("on_session_started requestId=" + session_started_request['requestId']
+          + ", sessionId=" + session['sessionId'])
+
+
+def on_launch(launch_request, session):
+    """ Called when the user launches the skill without specifying what they
+    want
+    """
+
+    print("on_launch requestId=" + launch_request['requestId'] +
+          ", sessionId=" + session['sessionId'])
+    # Dispatch to your skill's launch
+    return get_welcome_response()
+
+
+def on_intent(intent_request, session):
+    """ Called when the user specifies an intent for this skill """
+
+    print("on_intent requestId=" + intent_request['requestId'] +
+          ", sessionId=" + session['sessionId'])
+
+    intent = intent_request['intent']
+    intent_name = intent_request['intent']['name']
+
+    # Dispatch to your skill's intent handlers
+    if intent_name == "MyColorIsIntent":
+        return set_color_in_session(intent, session)
+    elif intent_name == "WhatsMyColorIntent":
+        return get_color_from_session(intent, session)
+    elif intent_name == "AMAZON.HelpIntent":
+        return get_welcome_response()
+    elif intent_name == "AMAZON.CancelIntent" or intent_name == "AMAZON.StopIntent":
+        return handle_session_end_request()
+    else:
+        raise ValueError("Invalid intent")
+
+
+def on_session_ended(session_ended_request, session):
+    """ Called when the user ends the session.
+
+    Is not called when the skill returns should_end_session=true
+    """
+    print("on_session_ended requestId=" + session_ended_request['requestId'] +
+          ", sessionId=" + session['sessionId'])
+    # add cleanup logic here
+
+
+# --------------- Main handler ------------------
+
+def lambda_handler(event, context):
+    """ Route the incoming request based on type (LaunchRequest, IntentRequest,
+    etc.) The JSON body of the request is provided in the event parameter.
+    """
+    print("event.session.application.applicationId=" +
+          event['session']['application']['applicationId'])
+
+    """
+    Uncomment this if statement and populate with your skill's application ID to
+    prevent someone else from configuring a skill that sends requests to this
+    function.
+    """
+    # if (event['session']['application']['applicationId'] !=
+    #         "amzn1.echo-sdk-ams.app.[unique-value-here]"):
+    #     raise ValueError("Invalid Application ID")
+
+    if event['session']['new']:
+        on_session_started({'requestId': event['request']['requestId']},
+                           event['session'])
+
+    if event['request']['type'] == "LaunchRequest":
+        return on_launch(event['request'], event['session'])
+    elif event['request']['type'] == "IntentRequest":
+        return on_intent(event['request'], event['session'])
+    elif event['request']['type'] == "SessionEndedRequest":
+        return on_session_ended(event['request'], event['session'])
